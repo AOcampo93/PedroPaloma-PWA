3dd1981e2f7f8ff6fd4f570db7520c7754821657
ArturoOcampo93 <arturo.ocampo@tvp.mx>
2020-01-07 17:14:53 -0600

    json

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,5 +1,5 @@
-// CALLING THE JSON
-const requestURL = 'producto.json';
+// LLmando el JSON
+const requestURL = 'https://aocampo93.github.io/PedroPaloma-PWA/js/producto.json';
 fetch(requestURL)
     .then(function(response) {
         return response.json();
@@ -7,20 +7,10 @@ fetch(requestURL)
     .then(function(jsonObject) {
         console.table(jsonObject);
         myobj = jsonObject;
-
-        // SEARCHING THE PROPERTIES IN THE JSON FILE
-
-        var name = myobj.paquetes[0].name;
-        //IMG
-        /*var FishPhoto = myobj.towns[1].photo;
-        var PrestonPhoto = myobj.towns[4].photo;
-        var SodaPhoto = myobj.towns[5].photo;*/
-
-
-        // PRINTING THE RESULTS IN THE HTML
-
-        //PHOTO
-        /*document.getElementById("FishPhoto").src = FishPhoto;
-        document.getElementById("PrestonPhoto").src = PrestonPhoto;
-        document.getElementById("SodaPhoto").src = SodaPhoto;*/
+        return myobj;
     });
+//Creando Tarjetas de paquetes
+function tarjetas(myobj) {
+    var paquetes = myobj['paquetes'];
+    document.getElementById('catalogo').innerHTML = paquetes[0].nombre;
+}
