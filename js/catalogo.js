428ed963256816e33bbbb312699a694d21e90add
ArturoOcampo93 <arturo.ocampo@tvp.mx>
2020-01-10 12:12:31 -0600

    Modificaciones JSON

--- js/catalogo.js
+++ js/catalogo.js
@@ -15,12 +15,30 @@ fetch(requestURL)
         for (var i = 0; i < paquetes.length; i++) {
             //Genera una etiqueta <article> Donde se almacenara toda la info
             var caja = document.createElement('article');
+            //Genera un div para almacenar la etiqueta <img>
+            var cajaImg = document.createElement('div');
+            //Genera la etiqueta <img>
+            var img = document.createElement('img');
+            //Genera un div para almacenar la info
+            var cajaInfo = document.createElement('div');
             //Genera un <h2> que sera el titulo de la tarjeta
             var titulo = document.createElement('h2');
             //Genera una etiqueta <p> que tendra la descripción
             var descripcion = document.createElement('p');
             //Genera una etiqueta <p> que tendra el precio
             var precio = document.createElement('p');
+            //Genera una etiqueta <button>
+            var boton = document.createElement('button');
+
+
+            //Decimos que dentro de <section> meta el <article> que hemos creado
+            section.appendChild(caja);
+
+            //Decimos que dentro de <article> meta los dos <div> empezando por el div "img"
+            caja.appendChild(cajaImg);
+            caja.appendChild(cajaInfo);
+            //Decimos que dentro del <div> "cajaImg" coloque la etiqueta <img>
+            cajaImg.appendChild(img);
 
             //Buscando info en el JSON para poner en la etiqueta correspondiente
             //Seleccionamos la etiqueta deseada -> seleccionamos la info en el JSON deseada
@@ -30,17 +48,29 @@ fetch(requestURL)
             descripcion.textContent = 'descripcion: ' + paquetes[i].descripcion;
             //p -> Precio
             precio.textContent = 'precio: ' + paquetes[i].precio;
+            //Escribimos dentro del botón el texto deseado
+            boton.textContent = 'Perzonaliza tu paquete';
+            var link = paquetes[i].link;
+            boton.setAttribute("onclick", "location.href=" + "'" + link + "'");
+
+
+            //Agregamos un src a la etiqueta <img> donde llamaremos desde el JSON la url de la imagen
+            img.src = 'images/' + paquetes[i].foto;
 
             //Le decimos que toda esta info la coloque en el interior de nuestra etiqueta <article>
-            caja.appendChild(titulo);
-            caja.appendChild(descripcion);
-            caja.appendChild(precio);
-            //Decimos que la seccion es la caja que hemos creado
-            section.appendChild(caja);
-
+            cajaInfo.appendChild(titulo);
+            cajaInfo.appendChild(descripcion);
+            cajaInfo.appendChild(precio);
+            cajaInfo.appendChild(boton);
 
             //Agregamos una clase al elemento "Caja"
             caja.classList.add('caja');
+            //Agregamos una clase al elemento "img-fluid" de Bootstrap
+            img.classList.add('img-fluid');
+
+
+
+
 
             //El for hará esto por el tamaño de Array[] del JSON
 
